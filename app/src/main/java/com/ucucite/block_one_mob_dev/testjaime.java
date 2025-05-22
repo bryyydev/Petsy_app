20072d02402c2c92ee93fcc7e7cbb9e6b60c6a4e
Jaime Yee <dev.yee29@gmail.com>
2025-05-23 02:24:54 +0800

    test

---
+++ app/src/main/java/com/ucucite/block_one_mob_dev/testjaime.java
@@ -0,0 +1,14 @@
+package com.ucucite.block_one_mob_dev;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class testjaime extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_testjaime);
+    }
+}
