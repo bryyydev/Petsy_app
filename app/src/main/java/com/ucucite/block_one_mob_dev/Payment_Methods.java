f2e9cc4fe3a8caed444bb98ad9061055f5ba8112
BOGZ <dev.bugayongb@gmail.com>
2025-05-29 02:23:46 +0800

    payment

---
+++ app/src/main/java/com/ucucite/block_one_mob_dev/Payment_Methods.java
@@ -0,0 +1,13 @@
+package com.ucucite.block_one_mob_dev;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Payment_Methods extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_payment_methods);
+    }
+}
